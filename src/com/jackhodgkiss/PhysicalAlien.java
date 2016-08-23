7b3ce2788e472163570ea068b51d47b4ede5b101
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-08-23 11:54:09 +0100

    Removed unused import.

--- src/com/jackhodgkiss/PhysicalAlien.java
+++ src/com/jackhodgkiss/PhysicalAlien.java
@@ -2,7 +2,6 @@ package com.jackhodgkiss;
 
 import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.PolygonShape;
-import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
