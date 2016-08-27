f344e75d9cc9dc79cc65c867e6da8636b4d4d251
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-08-27 12:48:58 +0100

    Level class implemented.
    This is a basic form of the Level class and is purely done now to
    simplyify the Main class.

---
+++ src/com/jackhodgkiss/Level.java
@@ -0,0 +1,75 @@
+package com.jackhodgkiss;
+
+import java.util.ArrayList;
+
+import org.jbox2d.dynamics.World;
+
+import processing.core.PApplet;
+
+public class Level {
+
+	private World world;
+	
+	private ArrayList<Actor> actors = new ArrayList<>();
+	private ArrayList<Actor> actorsToRemove = new ArrayList<>();
+	
+	private PhysicalAlien alien;
+	
+	private Background background;
+	
+	private PApplet parent;
+	
+	public Level(PApplet parent) {
+		this.parent = parent;
+		background = new Background(parent);
+		world = PhysiVals.getWorldInstance();
+		world.setContactListener(new ContactListener());
+		actors.add(new Floor(parent.width / 2, parent.height + 5, parent.width, 10));
+		actors.add(new PhysicalElement(parent.width / 2 - (220 / 2) + 35, parent.height - 35, Element.Wood010, parent));
+		actors.add(new PhysicalElement(parent.width / 2 + (220 / 2) - 35, parent.height - 35, Element.Wood010, parent));
+		actors.add(new PhysicalElement(parent.width / 2, parent.height - 105, Element.Wood012, parent));
+		actors.add(new PhysicalElement(parent.width / 2 - (220 / 2) + 35, parent.height - 175, Element.Wood010, parent));
+		actors.add(new PhysicalElement(parent.width / 2 + (220 / 2) - 35, parent.height - 175, Element.Wood010, parent));
+		actors.add(new PhysicalElement(parent.width / 2, parent.height - 245, Element.Wood012, parent));
+		alien = new PhysicalAlien(parent.width / 2, parent.height - 315, Alien.values()[(int) parent.random(0, Alien.values().length)], parent);
+		actors.add(alien);
+	}
+		
+	public void update() {
+		world.step(1.0f / 60.0f, 8, 3);
+		if(parent.mousePressed == true) {
+			for(Actor a : actors) {
+				if(a instanceof PhysicalElement) {
+					if(a.body != null) {
+						if(a.getSprite().isMouseOver()) {
+							a.kill();
+						}
+					}
+				}
+			}
+		}
+		for(Actor a : actors) {
+			if(a.getBody() != null) {
+				a.update();
+			} else {
+				actorsToRemove.add(a);
+			}
+		}
+		if(alien.isTouchedGround()) {
+			if(alien.body != null) {
+				alien.kill();
+				System.out.println("You Failed The Level, Alien Hit The Ground!");
+			}
+		}
+		actors.removeAll(actorsToRemove);
+		actorsToRemove.clear();
+	}
+	
+	public void draw() {
+		background.draw();
+		for(Actor a : actors) {
+			a.draw();
+		}
+	}
+	
+}
--- src/com/jackhodgkiss/Main.java
+++ src/com/jackhodgkiss/Main.java
@@ -1,78 +1,26 @@
 package com.jackhodgkiss;
 
-import java.util.ArrayList;
-
-import org.jbox2d.dynamics.World;
-
 import processing.core.PApplet;
 
 public class Main extends PApplet {
 	
-	World world;
-	
-	ArrayList<Actor> actors = new ArrayList<>();
-	ArrayList<Actor> actorsToRemove = new ArrayList<>();
-	
-	PhysicalAlien alien;
-	
-	Background background;
+	Level level;
 	
 	public void settings() {
 		size(1280, 720);
 	}
 	
 	public void setup() {
-		background = new Background(this);
-		world = PhysiVals.getWorldInstance();
-		world.setContactListener(new ContactListener());
-		actors.add(new Floor(width / 2, height + 5, width, 10));
-		actors.add(new PhysicalElement(width / 2 - (220 / 2) + 35, height - 35, Element.Wood010, this));
-		actors.add(new PhysicalElement(width / 2 + (220 / 2) - 35, height - 35, Element.Wood010, this));
-		actors.add(new PhysicalElement(width / 2, height - 105, Element.Wood012, this));
-		actors.add(new PhysicalElement(width / 2 - (220 / 2) + 35, height - 175, Element.Wood010, this));
-		actors.add(new PhysicalElement(width / 2 + (220 / 2) - 35, height - 175, Element.Wood010, this));
-		actors.add(new PhysicalElement(width / 2, height - 245, Element.Wood012, this));
-		alien = new PhysicalAlien(width / 2, height - 315, Alien.values()[(int) random(0, Alien.values().length)], this);
-		actors.add(alien);
+		level = new Level(this);
 	}
 	
 	public void update() {
-		world.step(1.0f / 60.0f, 8, 3);
-		for(Actor a : actors) {
-			if(a.getBody() != null) {
-				a.update();
-			} else {
-				actorsToRemove.add(a);
-			}
-		}
-		if(alien.isTouchedGround()) {
-			if(alien.body != null) {
-				alien.kill();
-				System.out.println("You Failed The Level, Alien Hit The Ground!");
-			}
-		}
-		actors.removeAll(actorsToRemove);
-		actorsToRemove.clear();
+		level.update();
 	}
-	
-	public void mouseClicked() {
-		for(Actor a : actors) {
-			if(a instanceof PhysicalElement) {
-				if(a.body != null) {
-					if(a.getSprite().isMouseOver()) {
-						a.kill();
-					}
-				}
-			}
-		}
-	}
-	
+		
 	public void draw() {
 		update();
-		background.draw();
-		for(Actor a : actors) {
-			a.draw();
-		}
+		level.draw();		
 	}
 	
 	public static void main(String[] args) {
